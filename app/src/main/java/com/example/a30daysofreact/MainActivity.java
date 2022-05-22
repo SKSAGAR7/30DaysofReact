6d2c322ae3721541f3703a5bb07514c4d070951e
Sk Sagar <sagarsk205@gmail.com>
2022-05-22 09:30:04 +0530

    Button Add

--- app/src/main/java/com/example/a30daysofreact/MainActivity.java
+++ app/src/main/java/com/example/a30daysofreact/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.a30daysofreact;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
